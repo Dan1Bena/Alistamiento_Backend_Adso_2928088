a72ac5dac4103a18ced36a0592ba2114b0a7ad10
k4mimi <camilagmwz@gmail.com>
2025-10-25 10:27:39 -0500

    corrección de app

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express ();
 // Middlewares
 app.use (cors());
 app.use (express.json({limit: '2omb '})); //Recibe datos en formato JSON con limite 
-app.use (express.urlencoded({extended: true,limit: '2omb '})); //Recibe datos codificados 
+app.use (express.urlencoded({extended: true,limit: '20mb '})); //Recibe datos codificados 
 
 // Rutas
 app.use('/api/auth', require('./routes/auth.routes'));
